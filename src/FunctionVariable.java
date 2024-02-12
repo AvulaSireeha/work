9a1f2c0ef8d49dcda72c4671fa21870aaed4566b
dell <avulasireesha0628@gmail.com>
2024-02-12 15:47:17 +0530

    function

---
+++ src/FunctionVariable.java
@@ -0,0 +1,9 @@
+public class FunctionVariable {
+    public String variableName;
+
+    public FunctionVariable(String variableName) {
+        this.variableName = variableName;
+    }
+    System.out.println("Hi");
+
+}
