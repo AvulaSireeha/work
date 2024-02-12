71d8f7d1e078d5730540a41b0716a2c703ff851e
dell <avulasireesha0628@gmail.com>
2024-02-12 16:11:34 +0530

    instancevariable

---
+++ src/InstanceVariable.java
@@ -0,0 +1,9 @@
+public class InstanceVariable {
+    public String variableName;
+
+    public InstanceVariable(String variableName) {
+        this.variableName = variableName;
+    }
+
+    System.out.println("VAriable");
+}
